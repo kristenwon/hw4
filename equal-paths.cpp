7f7c0e85a083ce202508c9eaee193e89bf8a7283
Kristen Won <kjwon@usc.edu>
2024-03-23 17:46:51 -0700

    Equal Paths complete

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,26 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+bool equalPathsHelper(Node* root, int depth, int& expectedDepth){
+    if(root == NULL){
+        return true;
+    }
+    if(root->left == NULL && root->right == NULL){
+        if(expectedDepth == -1){
+            expectedDepth = depth;
+        }
+        else if(depth != expectedDepth){
+            return false;
+        }
+    }
 
+    return equalPathsHelper(root->left, depth+1, expectedDepth) && equalPathsHelper(root->right, depth+1, expectedDepth);
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    int expectedDepth = -1;
+    return equalPathsHelper(root, 0, expectedDepth);
 }
 
